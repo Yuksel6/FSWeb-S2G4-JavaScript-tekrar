75c5b2ed639d4dd8ba88e51137b4928d54778f9c
Yüksel Beypınar <beypinaryuksel@gmail.com>
2024-12-29 16:41:59 +0300

    gorev1

--- index.js
+++ index.js
@@ -54,6 +54,9 @@ function CemberinCevresi(yaricap) {
   return CemberinCevresi = 2 * 3.14 * yaricap;
 }
 
+function CemberinCevresi(5) {
+  return CemberinCevresi = 2 * 3.14 * yaricap;
+}
 /* (Oto test yok) Yukarıdaki CemberinCevresi fonksiyonunu yarıçap = 5 vererek aşağıda çalıştırıp, sonucu konsolda gözlemleyin (console.log)  */
 
 /* 	GÖREV 2:  
